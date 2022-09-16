aae0fdacfc3a6315aab19a520d81173b8b5128d3
Denis Ryzhkov <dgryzhkov@gmail.com>
2022-09-16 11:35:28 +0700

    configure

---
+++ src/main/java/com/dgryzhkov/spring/security/configuration/MySecurityInitializer.java
@@ -0,0 +1,10 @@
+package com.dgryzhkov.spring.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * @author Dgryzhkov
+ */
+public class MySecurityInitializer extends
+        AbstractSecurityWebApplicationInitializer {
+}
